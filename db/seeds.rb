2fad3a26039701a755f476ec81b770c761ba999b
syca.red@tamu.edu <syca@DESKTOP-P8R348A>
2024-02-28 23:05:09 -0600

    add movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,13 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
    :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-   :release_date => '13-Aug-2021'}
+   :release_date => '13-Aug-2021'},
+  {:title => 'TongWu is lazy', :rating => 'R',
+   :release_date => '28-Feb-2024'},
+  {:title => 'TongWu forgot homework', :rating => 'R',
+   :release_date => '28-Feb-2024'},
+  {:title => 'TongWu failed course', :rating => 'R',
+   :release_date => '28-Feb-2024'}
 ]
 
 more_movies.each do |movie|
